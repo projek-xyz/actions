c0e4dfe37fa0e21d80f40ac5f6f9b33de0cbbe0e
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-24 12:17:39 +0700

    feat(prepare): add initial implementation of change assesment
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/changed-files.js
+++ scripts/changed-files.js
@@ -1,15 +1,98 @@
 // @ts-check
 
-/** @param {import('@actions/github-script').AsyncFunctionArguments} _ */
-export function changedFiles({ core, exec }) {
+/**
+ * @typedef {object} AssesmentOpt
+ * @property {boolean} isPhp
+ * @property {boolean} isNode
+ * @property {NodePM} nodePM
+ */
+
+/**
+ * @typedef {object} Assesment
+ * @property {boolean} build
+ * @property {boolean} deploy
+ * @property {boolean} tests
+ * @property {string} report
+ */
+
+/**
+ * @typedef {'bun'|'npm'|'pnpm'} NodePM
+ */
+
+/** @type {Record<NodePM, string[]>} */
+const pmPatterns = {
+  bun: ['bun.lock', 'bunfig.toml'],
+  npm: ['package-lock.json'],
+  pnpm: ['pnpm-lock.y?aml'],
+}
+
+/** @type {string[]} */
+const jsPatterns = [
+  '**/*.?(c|m)[jt]s?(x)',
+  'package.json',
+];
+
+/** @type {string[]} */
+const phpPatterns = [
+  '**/*.php',
+  'composer.*',
+  'phpunit.xml',
+];
+
+/**
+ * @param {import('minimatch')} minimatch
+ * @param {import('@actions/github-script').AsyncFunctionArguments} p2
+ */
+export function changedFiles(minimatch, { context, core, exec }) {
   /** @type {string[]} */
   const files = [];
 
   /**
+   * @param {string[]} files
+   * @param {AssesmentOpt} p0
+   * @returns {Assesment}
+   */
+  function getAssesment(files, { isPhp, isNode, nodePM }) {
+    const assesment = { build: false, deploy: false, tests: false, report: '' };
+
+    if (isPhp) {
+      for (const phpPatt of phpPatterns) {
+        if (files.some(minimatch.filter(phpPatt))) {
+          assesment.tests = true;
+          break;
+        }
+      }
+    }
+
+    if (isNode) {
+      jsPatterns.push(...pmPatterns[nodePM])
+
+      for (const jsPatt of jsPatterns) {
+        if (files.some(minimatch.filter(jsPatt))) {
+          assesment.tests = true;
+          break;
+        }
+      }
+    }
+
+    const reports = []
+
+    for (const [key, val] of Object.entries(assesment)) {
+      reports.push(`${key}: \u001b[1m${val}\u001b[0m`)
+    }
+
+    assesment.report = reports.join(', ')
+
+    return assesment
+  }
+
+
+  /**
    * @param {string} current
    * @param {string} previous
+   * @param {AssesmentOpt} opt
    */
-  return async function main(current, previous) {
+  return async function main(current, previous, opt) {
     await exec.exec('git', ['diff', '--name-only', current, previous], {
       ignoreReturnCode: true,
       silent: true,
@@ -30,6 +113,18 @@ export function changedFiles({ core, exec }) {
     }
 
     core.notice(`Found \u001b[1m${files.length}\u001b[0m files changed from ${current} to ${previous}`);
+
+    /** @type {Assesment} */
+    const assesment = context.eventName === 'workflow_dispatch'
+      ? { build: true, deploy: true, tests: true, report: 'Runs from `workflow_dispatch`' }
+      : getAssesment(files, opt)
+
+    core.notice(`Assesment: ${assesment.report}`);
+
+    core.setOutput('should-build', assesment.build ? 1 : 0);
+    core.setOutput('should-deploy', assesment.deploy ? 1 : 0);
+    core.setOutput('should-tests', assesment.tests ? 1 : 0);
+
     core.setOutput('available', files.length ? 1 : 0);
 
     return files;
